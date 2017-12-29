60751c8a9c2f74656cac3e2fc50e28901b9391bd
dipu-bd <dipu.sudipta@gmail.com>
2017-12-30 03:41:23 +0600

    Fix issues

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-require('./skycons')
-
-for (var i = iconList.length; i--; )
-  icons.set(list[i], list[i]);
-
-icons.play();
-
 module.exports.install = function (Vue, options) {
-
+  // initialize skycons
+  require('./skycons')
   var skycons = new Skycons({ color: options.color || 'black' });
+  skycons.play();
 
+  // register component
   Vue.component('skycon', {
-    template: '<canvas ref="canvas" :width="width" :height="height"></canvas>',
+    template: `<canvas ref="canvas" :width="width" :height="height"></canvas>`,
     props: {
       width: {
         type: Number,
@@ -61,21 +57,19 @@ module.exports.install = function (Vue, options) {
       }
     },
     mounted() {
-      skycons.set(this.$refs.canvas, Skycons[this.getType()]);
+      skycons.set(this.$refs.canvas, Skycons[this.type]);
     },
     computed: {
       type() {
         if(this.clear) return 'CLEAR' + (this.night ? '_NIGHT' : '_DAY')
         if(this.partlyCloudy) return 'PARTLY_CLOUDY' + (this.night ? '_NIGHT' : '_DAY')
-        if(this.fog) return 'FOG';
-        if(this.wind) return 'WIND';
-        if(this.snow) return 'SNOW';
-        if(this.sleet) return 'SLEET';
-        if(this.rain) return 'RAIN';
         if(this.cloudy) return 'CLOUDY';
+        if(this.rain) return 'RAIN';
+        if(this.sleet) return 'SLEET';
+        if(this.snow) return 'SNOW';
+        if(this.wind) return 'WIND';
+        if(this.fog) return 'FOG';
       }
     }
   })
-
-  skycons.play()
 }
